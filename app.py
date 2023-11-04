c88b3d83d01368382801e898c253df02d05b6e7e
vaibhav7766 <vaibhavsharmav777@gmail.com>
2023-11-04 18:18:38 +0530

    modified jupyter notebook and created app.py

--- app.py
+++ app.py
@@ -3,26 +3,23 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
+import pickle as pk
 
 # Load your dataset here
-x = pd.read_excel("output2019_2020_2021.xlsx")
-df = pd.DataFrame(x)
 
 # Split the data into features and target
-X = df[[2019, 2020]]
-y = df[2021]
 
-# Create and train the linear regression model
-model = LinearRegression()
-model.fit(X, y)
+
+with open("model.pkl","rb") as file:
+    model=pk.load(file)
 
 # Streamlit app starts here
 st.title('Linear Regression Model Deployment')
 
 # Add an input form for user input
 st.header('Input Values')
-feature1 = st.number_input('2019', min_value = 0.0, step = 0.01)
-feature2 = st.number_input('2020', min_value = 0.0, step = 0.01)
+feature1 = st.number_input('2019', min_value = 0, step = 1)
+feature2 = st.number_input('2020', min_value = 0, step = 1)
 
 # Make predictions with the trained model
 if st.button('Predict'):
@@ -34,11 +31,3 @@ if st.button('Predict'):
 
     st.subheader('Prediction:')
     st.write(f'Predicted Target: {prediction[0]:.2f}')
-
-# Add an evaluation section (optional)
-st.header('Model Evaluation')
-y_pred = model.predict(X)
-mse = mean_squared_error(y, y_pred)
-r2 = r2_score(y, y_pred)
-st.write(f'Mean Squared Error (MSE): {mse:.2f}')
-st.write(f'R-squared (R2) Score: {r2:.2f}')
